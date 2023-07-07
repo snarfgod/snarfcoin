97c08b6088e6c7fdc1b6a72649a7a433c37d32b8
snarfgod <dave.ports.1@gmail.com>
2023-07-07 09:12:15 -0400

    Smoke Test

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,5 +4,9 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    localhost: {},
+  },
 };
+
 
---
+++ scripts/1_deploy.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Token = await ethers.getContractFactory("Token");
+  const token = await Token.deploy();
+  
+  await token.deployed();
+
+  console.log("Token deployed to:", token.address);
+
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
